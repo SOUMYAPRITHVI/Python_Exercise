c35495c0e3be79e1283b0e09bdf3e815c4c96581
soumya <soumyakn89@gmail.com>
2023-12-12 11:13:12 +0530

    Add Exe39_session.py Exe39.py files

---
+++ Exe39_session.py
@@ -0,0 +1,14 @@
+stuff = {'name':'Zed','age':39,'height':6 * 12 + 2}
+print(stuff['name'])
+print(stuff['age'])
+print(stuff['height'])
+stuff['city']='SF'
+print(stuff['city'])
+stuff[1]="Wow"
+stuff[2]="Neato"
+print(stuff[1])
+print(stuff[2])
+print(stuff.pop('city'))
+print(stuff.pop(1))
+print(stuff.pop(2))
+print(stuff)
